b79b920a1d1b9b600d48056dae18ee09c2633583
jimbucktoo <jamesliang.g@gmail.com>
2021-04-13 12:55:18 -0500

    critical contract deployment address correction

--- ethereum/factory.js
+++ ethereum/factory.js
@@ -3,7 +3,7 @@ import CampaignFactory from "./build/CampaignFactory.json";
 
 const instance = new web3.eth.Contract(
     JSON.parse(CampaignFactory.interface),
-    "0x96761a602Dd950f62A3927663070565e9Fb2FC5b"
+    "0x4A4b650dA1800C7881F460C6b2EF849869c19f54"
 );
 
 export default instance;
