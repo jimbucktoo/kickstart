b8a59e3814b0645fa8730f63ee530f1f04d7335b
jimbucktoo <jamesliang.g@gmail.com>
2021-04-14 11:29:52 -0500

    critical next.js server side rendering window object correction

--- ethereum/web3.js
+++ ethereum/web3.js
@@ -1,5 +1,14 @@
 import Web3 from "web3";
 
-const web3 = new Web3(window.web3.currentProvider);
+let web3;
+
+if (typeof window !== "undefined" && typeof window.web3 !== "undefined") {
+    web3 = new Web3(window.web3.currentProvider);
+} else {
+    const provider = new Web3.providers.HttpProvider(
+        "https://rinkeby.infura.io/v3/023491f54c8c4fdc9c16956f522fd9b8"
+    );
+    web3 = new Web3(provider);
+}
 
 export default web3;
